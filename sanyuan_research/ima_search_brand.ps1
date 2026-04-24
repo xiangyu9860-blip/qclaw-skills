21a0b590da6bb24f02e0aca514f8ad2e08dced39
xiangyu9860-blip <xiangyu9860@gmail.com>
2026-04-24 11:03:40 +0800

    Add sanyuan_research/ima_search_brand.ps1

---
+++ sanyuan_research/ima_search_brand.ps1
@@ -0,0 +1,17 @@
+﻿$creds = & "D:/QQBrowser/QClaw/resources/openclaw/config/skills/ima/get-token.ps1" | ConvertFrom-Json
+$headers = @{
+    "ima-openapi-clientid" = $creds.client_id
+    "ima-openapi-apikey"   = $creds.api_key
+    "Content-Type" = "application/json"
+}
+$body = @{
+    query = "三元"
+    cursor = ""
+    limit = 20
+} | ConvertTo-Json -Depth 5
+
+$utf8Bytes = [System.Text.Encoding]::UTF8.GetBytes($body)
+$result = Invoke-RestMethod -Uri "https://ima.qq.com/openapi/wiki/v1/search_knowledge_base" `
+    -Method Post -Headers $headers `
+    -Body $utf8Bytes -ContentType "application/json; charset=utf-8"
+$result | ConvertTo-Json -Depth 20
